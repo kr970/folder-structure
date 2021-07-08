eda844938c1b44ec6949944fb23bde6f35a05cfb
Karyna Adamova <karina.dmd.1@gmail.com>
2021-07-08 19:34:53 +0300

    repeated parts refactored, added new folder option, rename bug fixed

--- app.js
+++ app.js
@@ -54,26 +54,60 @@ const divFileStructure = document.getElementById('file-structure');
 const contextMenu = document.getElementById('context-menu');
 const renameOption = document.getElementById('rename');
 const deleteOption = document.getElementById('delete');
+const createOption = document.getElementById('create');
 const canvas = document.getElementById('canvas');
+const createRootFolderOption = document.getElementById('create-root-folder');
 let currentItem;
 let name;
-
+createOption.addEventListener('click', () => createNewFolder(fileStructure, currentItem.value));
+createRootFolderOption.addEventListener('click',() => createRootFolder(fileStructure));
 renameOption.addEventListener('click', (e) => renameFile(e, fileStructure));
 deleteOption.addEventListener('click', () => deleteFile(fileStructure));
 
-function enterFileName(fileStructureItems, pastName) {
-    for (let i = 0; i < fileStructureItems.length; i++) {
-      if (fileStructureItems[i].name === pastName) {
+function createRootFolder(fileStructure) {
+  fileStructure.push({ isOpen: false, extension: 'folder', name: 'New folder', content: []});
+  deleteFileStructure();
+  drawFileStructure(fileStructure, divFileStructure,1);
+}
+
+function traverseFileStructure(fileStructureItems, itemValue, option) {
+  for (let i = 0; i < fileStructureItems.length; i++) {
+    if (fileStructureItems[i].name === itemValue) {
+      if (option === 'createNewFolder') {
+        fileStructureItems[i].content.push({ isOpen: false, extension: 'folder', name: 'New folder', content: []});
+      } else if (option === 'enterFileName') {
         fileStructureItems[i].name = name;
-        return;
+      } else if (option === 'deleteFile') {
+        fileStructureItems.splice(i, 1);
       }
-      if (fileStructureItems[i].content && fileStructureItems[i].content.length > 0) {
-        enterFileName(fileStructureItems[i].content, pastName)
-      }
+      return;
     }
+    if (fileStructureItems[i].content && fileStructureItems[i].content.length > 0) {
+      traverseFileStructure(fileStructureItems[i].content, itemValue, option);
+    }
+  }
+}
+
+function createNewFolder(fileStructure, name) {
+  contextMenu.setAttribute('hidden', 'hidden');
+  traverseFileStructure(fileStructure, name, 'createNewFolder');
+  deleteFileStructure();
+  drawFileStructure(fileStructure,divFileStructure,1);
+}
+
+function addCanvas() {
+  contextMenu.style.zIndex = '2';
+  canvas.style.zIndex = '1';
+  divFileStructure.style.zIndex = '0';
+}
+
+function deleteCanvas() {
+  canvas.style.zIndex = '0';
+  divFileStructure.style.zIndex = '1';
 }
 
 function renameFile(e, fileStructureItems) {
+  addCanvas();
   let prevName = currentItem.value;
   currentItem.removeAttribute('readonly');
   currentItem.focus();
@@ -82,31 +116,25 @@ function renameFile(e, fileStructureItems) {
   const onInput = () => name = currentItem.value;
   currentItem.addEventListener('input', onInput);
   const onBlur = () => {
-    enterFileName(fileStructureItems, prevName);
+    traverseFileStructure(fileStructureItems, prevName, 'enterFileName');
     deleteFileStructure();
-    drawFileStructure(fileStructure,divFileStructure,1);
+    drawFileStructure(fileStructure, divFileStructure,1);
     currentItem.removeEventListener('input', onInput);
     currentItem.removeEventListener('blur', onBlur);
+    deleteCanvas();
   };
   currentItem.addEventListener('blur', onBlur);
 }
 
 function deleteFile(fileStructureItems) {
   contextMenu.setAttribute('hidden', 'hidden');
-  for (let i = 0; i < fileStructureItems.length; i++) {
-    if (fileStructureItems[i].name === currentItem.value) {
-      fileStructureItems.splice(i, 1);
-      deleteFileStructure();
-      drawFileStructure(fileStructure,divFileStructure,1);
-      return;
-    }
-    if (fileStructureItems[i].content && fileStructureItems[i].content.length > 0) {
-      deleteFile(fileStructureItems[i].content)
-    }
-  }
+  traverseFileStructure(fileStructureItems, currentItem.value, 'deleteFile');
+  deleteFileStructure();
+  drawFileStructure(fileStructure, divFileStructure,1);
 }
 
 function handleContextMenu(e) {
+  console.log('handleContextMenu');
   currentItem = e.target;
   e.stopPropagation();
   if(e.button === 2) {
@@ -129,9 +157,8 @@ document.body.addEventListener('contextmenu', function(e) {
   e.preventDefault();
 }, false);
 
-function checkIsOpen(folder) {
-  if(!folder.isOpen) return 'pictures/folder_close.svg';
-  return 'pictures/folder_open.svg';
+function checkIsOpen(elem, folder) {
+  folder.isOpen ? elem.classList.add('folder-open') :  elem.classList.add('folder-close');
 }
 
 function doIndent(container, nesting) {
@@ -145,43 +172,45 @@ function deleteFileStructure() {
   }
 }
 
-function onFolderClick(e,folder) {
+function onFolderClick(e, folder) {
   if(e.which === 1) {
     folder.isOpen = !folder.isOpen;
   }
   let img = document.getElementById(folder.name).firstChild;
-  img.src = checkIsOpen(folder);
+  e.target.classList.add(fileStructure.isOpen ? 'folder-open' : 'folder-close');
 
   deleteFileStructure();
-  drawFileStructure(fileStructure,divFileStructure,1)
+  drawFileStructure(fileStructure, divFileStructure,1)
 }
 
-function createContainerWithFile(fileStructureName, fileStructure, eventListener, imageSrc, folderNesting, fileStructureExtension = '') {
-  let container = document.createElement('div');
-  container.classList.add ('container');
-  container.id = fileStructureName + fileStructureExtension;
+function createElementWithFile(fileStructure, eventListener, folderNesting) {
+  let element = document.createElement('div');
+  element.classList.add(fileStructure.extension === 'folder' ? 'folder' : 'file');
+  element.id = fileStructure.name;
   if (eventListener) {
-    container.addEventListener('click', (e) => eventListener(e, fileStructure));
+    element.addEventListener('click', (e) => eventListener(e, fileStructure));
   }
   let input = document.createElement('input');
-  input.value = fileStructureName;
+  input.value = fileStructure.name;
   input.classList.add('file-name');
   input.setAttribute('readonly', 'readonly');
   let img = document.createElement('img');
-  img.src = imageSrc;
+  if(fileStructure.extension === 'folder') {
+    element.classList.add(fileStructure.isOpen ? 'folder-open' : 'folder-close');
+  }
   img.classList.add('file-logo');
-  container.append(img);
-  container.append(input);
-  doIndent(container, folderNesting);
-  return container;
+  element.append(img);
+  element.append(input);
+  doIndent(element, folderNesting);
+  return element;
 }
 
-function drawFileStructure(fileStructure,parentFolder,folderNesting ) {
+function drawFileStructure(fileStructure, parentFolder, folderNesting ) {
   for(let i = 0; i < fileStructure.length; i++) {
     if (fileStructure[i].extension === 'folder') {
       let div = document.createElement('div');
-      let container = createContainerWithFile(fileStructure[i].name, fileStructure[i], onFolderClick, checkIsOpen(fileStructure), folderNesting);
-      div.append(container);
+      let element = createElementWithFile(fileStructure[i], onFolderClick, folderNesting);
+      div.append(element);
       parentFolder.append(div);
 
       if(!fileStructure[i].isOpen) {
@@ -189,7 +218,7 @@ function drawFileStructure(fileStructure,parentFolder,folderNesting ) {
       }
 
       if (fileStructure[i].content.length > 0) {
-        drawFileStructure(fileStructure[i].content,div,folderNesting+1);
+        drawFileStructure(fileStructure[i].content, div, folderNesting+1);
       } else {
           let i = document.createElement('i');
           i.innerHTML = 'Folder is empty';
@@ -197,8 +226,8 @@ function drawFileStructure(fileStructure,parentFolder,folderNesting ) {
           doIndent(i, folderNesting + 1);
         }
     } else {
-      let container = createContainerWithFile(fileStructure[i].name, fileStructure[i], null, 'pictures/file.svg', folderNesting, fileStructure[i].extension);
-      parentFolder.append(container);
+      let element = createElementWithFile(fileStructure[i], null, folderNesting);
+      parentFolder.append(element);
     }
   }
 }
